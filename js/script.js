174daa90840fb25ced4c52a109f31252ebef438b
Kayahs <akshaykmanchanda@gmail.com>
2018-10-09 10:18:09 -0400

    Footer completed, started work on add to cart function

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ $(document).ready(function(){
 		prevNextButtons: false
 	});
 });
+
+function incrementCart() {
+	var cartCountElem = document.getElementById('cart-item-count');
+	console.log(cartCountElem);
+	var currentCount = parseInt(cartCountElem[0].innerHTML);
+	console.log(cartCountElem.innerHTML);
+	console.log(currentCount);
+	var newCount = currentCount++;
+	cartCountElem.innerHTML = newCount;
+}
